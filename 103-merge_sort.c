b28dbc711b14c0570884821e82e5fef412a59e76
mostafa bukhadra <mvxizk@gmail.com>
2024-02-18 22:00:14 +0200

    ok

---
+++ 103-merge_sort.c
@@ -0,0 +1,7 @@
+#include "sort.h"
+
+void merge_sort(int *array, size_t size)
+{
+
+
+}
--- sort.h
+++ sort.h
@@ -21,8 +21,14 @@ typedef struct listint_s
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+
 void bubble_sort(int *array, size_t size);
+
 void insertion_sort_list(listint_t **list);
 void sort_any_where(listint_t *to_sort, listint_t *ptr2);
+
 void selection_sort(int *array, size_t size);
+
+void merge_sort(int *array, size_t size);
+
 #endif
